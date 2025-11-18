7a43ab452390476bc5317a74de183843f0420146
Mariam Hagras <mariamhagras99@gmail.com>
2025-11-18 16:55:45 +0200

    fix

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { assets, cities } from '../assets/assets'
-import { useAppContext } from '../context/appContext';
+import { useAppContext } from '../context/AppContext';
 
 const Hero = () => {
   const {navigate,getToken,axios,setSearchedCities}=useAppContext();

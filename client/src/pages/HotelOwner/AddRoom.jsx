788936e4e06431a253b86b8156557ba4f5546572
Mariam Hagras <mariamhagras99@gmail.com>
2025-11-29 17:50:46 +0200

    fix wifi icon

--- client/src/pages/HotelOwner/AddRoom.jsx
+++ client/src/pages/HotelOwner/AddRoom.jsx
@@ -18,7 +18,7 @@ const AddRoom = () => {
     roomType:'',
     pricePerNight:0,
     amenities:{
-      'Free Wifi':false,
+      'Free WiFi':false,
       'Free Breakfast':false,
       'Room Service':false,
       'Mountain View':false,
@@ -74,7 +74,7 @@ const AddRoom = () => {
         roomType: '',
         pricePerNight: 0,
         amenities: {
-          'Free Wifi': false,
+          'Free WiFi': false,
           'Free Breakfast': false,
           'Room Service': false,
           'Mountain View': false,

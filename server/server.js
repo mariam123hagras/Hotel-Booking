a499066f46e67d67b28632ba5399b957e46a7cf0
Mariam Hagras <mariamhagras99@gmail.com>
2025-12-04 19:38:13 +0200

    Project Done emoji 🎉✨🥳

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(clerkMiddleware())
 
 // API to listen to Clerk Webhooks
 app.use('/api/clerk',clerkWebhooks);
-app.get('/',(req,res)=>res.send('ApI is working'))
+app.get('/',(req,res)=>res.send('API is working'))
 app.use('/api/user',userRouter)
 app.use('/api/hotels',hotelRouter)
 app.use('/api/rooms',roomRouter)
